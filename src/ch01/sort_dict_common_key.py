ad29b7658b1498956dbd6f2d7c9e83c860ef7acb
MiketheViking90 <siliwen@gmail.com>
2019-12-08 15:05:53 -0800

    itemgetter and lambda to default if missing

--- src/ch01/sort_dict_common_key.py
+++ src/ch01/sort_dict_common_key.py
@@ -5,10 +5,13 @@ rows = [
     {'fname': 'Brian', 'lname': 'Jones', 'uid': 1003},
     {'fname': 'David', 'lname': 'Beazley', 'uid': 1002},
     {'fname': 'John', 'lname': 'Cleese', 'uid': 1001},
-    {'fname': 'Big', 'lname': 'Jones', 'uid': 1004}
+    {'fname': 'Big', 'lname': 'Jones', 'uid': 1004},
+    {'fname': 'Jill', 'lname': 'Thomas'},
+    {'fname': 'Nate', 'lname': 'Nonong'},
+    {'fname': 'Jesse', 'lname': 'Santiago'},
 ]
 
+by_id = sorted(rows, key=lambda x: x.get('uid', 0))
 by_lname = sorted(rows, key=itemgetter('lname'))
+pprint(by_id)
 pprint(by_lname)
-
-print(min(rows, key=itemgetter('lname')))
