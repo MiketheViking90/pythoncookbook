f749b03c37393c0f36ec8a8f606a6d2a31a90ea3
Jim Wen <jimwen@lyft.com>
2019-12-19 20:31:20 -0800

    start num recipes. round() for decimals and pow tens

---
+++ src/ch03/rounding_nums.py
@@ -0,0 +1,17 @@
+rounded = round(1.23, 1)
+print(rounded)
+
+rounded = round(1.5, 0)
+print(rounded)
+
+rounded = round(2.5, 0)
+print(rounded)
+
+rounded = round(15192823, -1)
+print(rounded)
+
+rounded = round(15192823, -2)
+print(rounded)
+
+rounded = round(15192823, -3)
+print(rounded)
