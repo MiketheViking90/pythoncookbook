84afff2b426d680b14cc036a56aed967d6612efa
MiketheViking90 <siliwen@gmail.com>
2019-12-20 16:08:45 -0800

    timedelta and relativedelta for time based comps

--- src/ch03/time_conversions.py
+++ src/ch03/time_conversions.py
@@ -1,5 +1,7 @@
 from datetime import timedelta, datetime
 
+from dateutil.relativedelta import relativedelta
+
 a = timedelta(days=2, hours=6)
 b = timedelta(hours=4.5)
 c = a+b
@@ -14,3 +16,9 @@ date_time = datetime(year=2019, month=1, day=30, hour=13, minute=23)
 print(date_time)
 print(date_time.timestamp())
 print(date_time + c)
+
+dt2 = date_time + relativedelta(months=+3, days=-3, minutes=+872782)
+print(dt2)
+
+diff = dt2-date_time
+print(diff)
